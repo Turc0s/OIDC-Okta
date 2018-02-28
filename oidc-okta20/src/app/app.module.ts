a78e5514a3743c9e2069d837240f97dbbec959c9
Tapio Mattila <tapsa87@gmail.com>
2018-02-28 12:49:48 +0200

    Added the basic search and edit functionality by ex

--- oidc-okta20/src/app/app.module.ts
+++ oidc-okta20/src/app/app.module.ts
@@ -1,22 +1,34 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { EditComponent } from './edit/edit.component';
 import { SearchComponent } from './search/search.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchService } from './shared';
+import { HttpModule } from '@angular/http';
+import { EditComponent } from './edit/edit.component';
 
+const appRoutes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'edit/:id', component: EditComponent},
+  {path: '', redirectTo: '/search', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditComponent,
-    SearchComponent
+    SearchComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
